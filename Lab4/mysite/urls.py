ae85836eb64e52606726ab7c6a30a9993d325226
= <childebr@ualberta.ca>
2022-10-03 19:02:11 -0600

    lab 4 done

--- Lab4/mysite/urls.py
+++ Lab4/mysite/urls.py
@@ -13,9 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.contrib import admin
+# from django.urls import include, path
+# from django import views
+
+# urlpatterns = [
+#     # path('admin/', admin.site.urls),
+#     path('polls/', include('polls.urls')),
+#     path('', views.index, name='index'),
+# ]
+
+
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
